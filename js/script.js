96bb55a85e59ab05da19018d3f815cad0419971e
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-18 18:35:49 +0100

    feat: add the score of user with a condition "if" inside the else condition. when "this" cell is clicked, and it's not a bomb, verify that the class "square-blue" is not included, if it's true, add the class "square-blue" and increase the userScore but if you click twice the same clicked cell, the score do not grow

--- js/script.js
+++ js/script.js
@@ -4,6 +4,22 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
+// SECONDA PARTE
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà
+// prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata 
+// al massimo una bomba, perciò nell’array delle bombe non potranno esserci 
+// due numeri uguali.
+
+// Funzione che genera 16 numeri casuale da un numero min ad un numero max
+// I numeri devono essere da 1 a 100 (ma anche renderli dinamici)
+// I numeri generati casualmente non devono essere uguali tra loro
+// Vanno pushati dentro un array 
+// Se i numeri generati dal ciclo sono uguali a quelli contenuti nell'array
+// Allora nel caso di questi numeri al momento del click della cella il colore diventa rossa
+// Il gioco termina  
+
+// Prendere ogni item all'interno dell'array e se è uguale al un numero presente nel ciclo for 
+// Mettere una condizione all'interno dell'evento di ascolto che assegna il colore alla cella
 
 const gridGame = document.querySelector('.grid');
 const playStartTheGame = document.querySelector('.btn');
@@ -22,13 +38,14 @@ playStartTheGame.addEventListener('click', function () {
    console.log(generatedRandomNumbers);
 
     // La variabile parte del presupposto che la bomba non è cliccata
-    let bombClicked = false
+    let yourScore = 0;
+    let bombClicked = false;
     for (let i = 1; i <= howManyCells; i++) {
         let numberCellsGrid = i
         let squareGenerated = squareGenerator(numberCellsGrid, mySelect)
         gridGame.append(squareGenerated);
 
-        // Se il numero è presente nell'array di generateBombItems allora il colore di bg è rosso
+        // Se il numero è presente nell'array di generateBombItems allora il colore di bg è rosso       
         squareGenerated.addEventListener('click', function() {
             // Con la condizione if gli diciamo che finché la bomba non è cliccata allora tutto il resto della condizione del condice si verifica
             if (!bombClicked) {
@@ -36,30 +53,21 @@ playStartTheGame.addEventListener('click', function () {
                     this.classList.add('square-red')
                     console.log(numberCellsGrid);
                     bombClicked = true // nel momento in cui la bomba viene cliccata la variabile in alto diventa true è il gioco termina
-                } else {
-                    this.classList.add('square-blue')
-                    console.log(numberCellsGrid);
-                } 
-            }
+                    console.log(yourScore);
+                } else {         
+                    // Il click viene contato solo se la cella non ha la classe 'square-blu'
+                    if (!this.classList.contains('square-blue')) {
+                        this.classList.add('square-blue')
+                        yourScore++
+                        console.log(yourScore);
+                    }
+                }
+            }           
         })
     }
 });
 
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà
-// prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata 
-// al massimo una bomba, perciò nell’array delle bombe non potranno esserci 
-// due numeri uguali.
 
-// Funzione che genera 16 numeri casuale da un numero min ad un numero max
-// I numeri devono essere da 1 a 100 (ma anche renderli dinamici)
-// I numeri generati casualmente non devono essere uguali tra loro
-// Vanno pushati dentro un array 
-// Se i numeri generati dal ciclo sono uguali a quelli contenuti nell'array
-// Allora nel caso di questi numeri al momento del click della cella il colore diventa rossa
-// Il gioco termina  
-
-// Prendere ogni item all'interno dell'array e se è uguale al un numero presente nel ciclo for 
-// Mettere una condizione all'interno dell'evento di ascolto che assegna il colore alla cella
 
 // FUNCTIONS
 // La funzione ha lo scopo di generare delle celle chiamate square con numeri all'interno da 1 a 100
